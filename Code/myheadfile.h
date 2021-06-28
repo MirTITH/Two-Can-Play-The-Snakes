db2d29b35b9527edda2c2c1c0390531547394170
TITH <1023237464@qq.com>
2021-06-28 22:18:07 +0800

    添加自定义光标

---
+++ Code/myheadfile.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "PainterEngine_Application.h"
+#include <Windows.h>
+
+typedef struct pointer_pos
+{
+	px_float x = 0;
+	px_float y = 0;
+}POINTER_POS;		
--- PainterEngine/project/PainterEngine_Application.cpp
+++ PainterEngine/project/PainterEngine_Application.cpp
@@ -1,9 +1,13 @@
-#include "PainterEngine_Application.h"
+#include "myheadfile.h"
+
 PX_Application App;
+POINTER_POS pt = { 0,0 };
+
 
 px_bool PX_ApplicationInitialize(PX_Application *pApp,px_int screen_width,px_int screen_height)
 {
 	PX_ApplicationInitializeDefault(&pApp->runtime, screen_width, screen_height);
+	ShowCursor(FALSE); //Òþ²ØwindowsÊó±ê
 	return PX_TRUE;
 }
 
@@ -15,13 +19,19 @@ px_void PX_ApplicationUpdate(PX_Application *pApp,px_dword elpased)
 px_void PX_ApplicationRender(PX_Application *pApp,px_dword elpased)
 {
 	px_surface *pRenderSurface=&pApp->runtime.RenderSurface;
-	PX_RuntimeRenderClear(&pApp->runtime, PX_COLOR(255, 255, 155, 125));
-	//test
+	//GetCursorPos(&pt);
+	//Sleep(1000/144);
+	PX_RuntimeRenderClear(&pApp->runtime, PX_COLOR(255, 0, 0, 0));
+	PX_GeoDrawCircle(pRenderSurface, pt.x, pt.y, 20, 5, PX_COLOR(255, 102, 204, 255));
 }
 
 px_void PX_ApplicationPostEvent(PX_Application *pApp,PX_Object_Event e)
 {
 	PX_ApplicationEventDefault(&pApp->runtime, e);
-
+	if (e.Event == PX_OBJECT_EVENT_CURSORMOVE)
+	{
+		pt.x = PX_Object_Event_GetCursorX(e);
+		pt.y = PX_Object_Event_GetCursorY(e);
+	}
 }
 
